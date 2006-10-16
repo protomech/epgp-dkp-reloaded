5957757f1a95e4117fef29ad7da09c5c4af71eef
evlogimenos <devnull@localhost>
2006-10-16 09:33:57 +0000

    Make script accept a version argument.

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ ! $# == 1 ]; then
+  echo "Usage: $0 <release_version>"
+  exit 1
+fi
+
 ORIGINAL_DIR=$PWD
 ADDON_DIR=$(dirname "$(readlink -f $0/..)")
 
@@ -7,5 +12,5 @@ cd /tmp
 cp -R "$ADDON_DIR" /tmp/
 find epgp -type d -name ".svn" | xargs rm -rf
 rm -rf epgp/scripts
-zip -r "$ORIGINAL_DIR"/epgp.zip epgp
+zip -r "$ORIGINAL_DIR"/epgp-$1.zip epgp
 rm -rf /tmp/epgp
