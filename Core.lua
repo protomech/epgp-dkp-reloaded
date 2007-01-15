3f403a5ae750227c10b526798693f8cb1cca2d64
evlogimenos <devnull@localhost>
2007-01-15 00:52:34 +0000

    Move GP value display on tooltips in a separate module.

--- Core.lua
+++ Core.lua
@@ -1,5 +1,4 @@
 EPGP = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0", "AceDB-2.0", "AceDebug-2.0", "AceEvent-2.0", "AceHook-2.1", "AceModuleCore-2.0", "FuBarPlugin-2.0")
-EPGP:SetModuleMixins("AceDebug-2.0")
 
 -------------------------------------------------------------------------------
 -- DB defaults
@@ -14,13 +13,6 @@ EPGP:RegisterDefaults("profile", {
   raid_mode = true,
 })
 
-local function OnTooltipSetItem(obj)
-	EPGP:Print("OnTooltipSetItem")
-	for k,v in pairs(obj) do
-		EPGP:Print(tostring(k)..": "..tostring(v))
-	end
-end
-
 -------------------------------------------------------------------------------
 -- Init code
 -------------------------------------------------------------------------------
@@ -33,69 +25,6 @@ function EPGP:OnInitialize()
 	self.MAX_POINTS_TOTAL = 999999999
   self.OnMenuRequest = self:BuildOptions()
   self:RegisterChatCommand({ "/epgp" }, self.OnMenuRequest)
-
-	self:SecureHook(GameTooltip, "SetAuctionItem", function(this, type, index) EPGP:AddGPValue(this, GetAuctionItemLink(type, index)) end)
-	self:SecureHook(GameTooltip, "SetBagItem", function(this, bag, slot) EPGP:AddGPValue(this, GetContainerItemLink(bag, slot)) end)
-  self:SecureHook(GameTooltip, "SetHyperlink", function(this, link) EPGP:AddGPValue(this, link) end)
-	self:SecureHook(GameTooltip, "SetInventoryItem", function(this, unit, slot) EPGP:AddGPValue(this, GetInventoryItemLink(unit, slot)) end)
-	self:SecureHook(GameTooltip, "SetLootItem", function(this, slot) EPGP:AddGPValue(this, GetLootSlotLink(slot)) end)
-	self:SecureHook(GameTooltip, "SetLootRollItem", function(this, slot) EPGP:AddGPValue(this, GetLootRollItemLink(slot)) end)
-	self:SecureHook(GameTooltip, "SetMerchantItem", function(this, index) EPGP:AddGPValue(this, GetMerchantItemLink(index)) end)
-	self:SecureHook(GameTooltip, "SetQuestItem", function(this, type, index) EPGP:AddGPValue(this, GetQuestItemLink(type, index)) end)
-	self:SecureHook(GameTooltip, "SetQuestLogItem", function(this, type, index) EPGP:AddGPValue(this, GetQuestLogItemLink(type, index)) end)
-	self:SecureHook(GameTooltip, "SetSendMailItem", function(this) EPGP:AddGPValue(this, GetSendMailItem()) end)
-	self:SecureHook(GameTooltip, "SetTradePlayerItem", function(this, id) EPGP:AddGPValue(this, GetTradePlayerItemLink(id)) end)
-	self:SecureHook(GameTooltip, "SetTradeSkillItem", function(this, index) EPGP:AddGPValue(this, GetTradeSkillItemLink(index)) end)
-	self:SecureHook(GameTooltip, "SetTradeTargetItem", function(this, id) EPGP:AddGPValue(this, GetTradeTargetItemLink(id)) end)
-  self:SecureHook(ItemRefTooltip, "SetHyperlink", function(this, link) EPGP:AddGPValue(this, link) end)
-	GameTooltip:SetScript("OnTooltipSetItem", OnTooltipSetItem)
-end
-
-local EQUIPSLOT_VALUE = {
-	["INVTYPE_HEAD"] = 1,
-	["INVTYPE_NECK"] = 0.55,
-	["INVTYPE_SHOULDER"] = 0.777,
-	["INVTYPE_CHEST"] = 1,
-	["INVTYPE_ROBE"] = 1,
-	["INVTYPE_WAIST"] = 0.777,
-	["INVTYPE_LEGS"] = 1,
-	["INVTYPE_FEET"] = 0.777,
-	["INVTYPE_WRIST"] = 0.55,
-	["INVTYPE_HAND"] = 0.777,
-	["INVTYPE_FINGER"] = 0.55,
-	["INVTYPE_TRINKET"] = 0.7,
-	["INVTYPE_CLOAK"] = 0.55,
-	["INVTYPE_WEAPON"] = 0.42,
-	["INVTYPE_SHIELD"] = 0.55,
-	["INVTYPE_2HWEAPON"] = 1,
-	["INVTYPE_WEAPONMAINHAND"] = 0.42,
-	["INVTYPE_WEAPONOFFHAND"] = 0.42,
-	["INVTYPE_HOLDABLE"] = 0.55,
-	["INVTYPE_RANGED"] = 0.42,
-	["INVTYPE_RANGEDRIGHT"] = 0.42
-}
-
-local ILVL_TO_IVALUE = {
-	[2] = function(ilvl) return (ilvl - 4) / 2 end,         -- Green
-	[3] = function(ilvl) return (ilvl - 1.84) / 1.6 end,   -- Blue
-	[4] = function(ilvl) return (ilvl - 1.3) / 1.3 end,     -- Purple
-}
-function EPGP:AddGPValue(frame, itemLink)
-	if not itemLink then return end
-	local name, link, rarity, level, minlevel, type, subtype, count, equipLoc = GetItemInfo(itemLink)
-	local islot_mod = EQUIPSLOT_VALUE[equipLoc]
-	if not islot_mod then return end
-	local ilvl2ivalue = ILVL_TO_IVALUE[rarity]
-	if ilvl2ivalue then
-		local ivalue = ilvl2ivalue(level)
-		local gp = math.floor(ivalue^2 * 0.04 * islot_mod)
-		if gp > 0 then
-			frame:AddDoubleLine("GP", string.format("%d", gp),
-				NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b,
-				NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b)
-			frame:Show()
-		end
-	end
 end
 
 function EPGP:OnEnable()
