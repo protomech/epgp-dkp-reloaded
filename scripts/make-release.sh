a1907e2611d8694a5646fc2b65a2b467c67df6f6
evlogimenos <devnull@localhost>
2007-03-25 00:42:10 +0000

    Fix release script to not include any hidden files.

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -14,7 +14,7 @@ EPGP_DIR=$PWD
 RELEASE_ZIP="$EPGP_DIR/epgp-$1.zip"
 
 pushd ..
-zip -r "$RELEASE_ZIP" epgp -x \*/.svn/\* -x \*/scripts/\* -x \*/wiki/\* -x \*~
+zip -r "$RELEASE_ZIP" epgp -x \*/.\* -x \*/scripts/\* -x \*/wiki/\* -x \*~
 popd
 
 unzip "$RELEASE_ZIP"
