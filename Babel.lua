6f33873624ae73091418fad65f2f0a7e6ed62117
evlogimenos <devnull@localhost>
2007-05-09 23:21:50 +0000

    Add localization builder.

---
+++ Babel.lua
@@ -0,0 +1,81 @@
+local strings = {}
+
+if #arg < 2 then
+  print("Usage: Babel.lua varname sourcefile ...\n"..
+        "  varname - the variable name of the global table holding the translations\n"..
+        "  sourcefile ... - the source files to scavenge for localized strings\n")
+end
+
+-- The first arg is the global variable to use for the global table
+local varname = arg[1]
+
+-- Parse all the lua files given in the command line and extract
+-- strings for localization.
+for i = 2,#arg do
+  local file = io.open(arg[i], "r")
+  assert(file, "Could not open "..arg[i])
+  print("Reading "..arg[i])
+  local text = file:read("*all")
+
+  for match in string.gmatch(text, "L%[\"(.-)\"%]") do
+    table.insert(strings, match)
+  end
+end
+
+local languages = {
+  "enUS",
+  "frFR",
+  "deDE",
+}
+
+local lang_tables = {}
+
+-- Populate tables and merge in existing Localization files
+for i,lang in pairs(languages) do
+  if not lang_tables[lang] then
+    lang_tables[lang] = {}
+  end
+
+  local lang_table = lang_tables[lang]
+
+  for i,str in pairs(strings) do
+    lang_table[str] = str
+  end
+  
+  local filename = "Localization."..lang..".lua"
+  local file = io.open(filename, "r")
+  if file then
+    print("Merging localizations from "..filename)
+    function GetLocale() return lang end
+    loadfile(filename)()
+    for k,v in pairs(_G[varname]) do
+      if lang_table[k] then
+        lang_table[k] = v
+      end
+    end
+  end
+end
+
+-- Output new localization files
+for i,lang in pairs(languages) do
+  local filename = "Localization."..lang..".lua"
+  local file = io.open(filename, "w")
+  assert(file, "Could not open "..filename.." for writing!")
+  print("Writing localizations to "..filename)
+  if lang ~= "enUS" then
+    file:write("if GetLocale() ~= \""..lang.."\" then return end\n")
+    file:write(varname.." = setmetatable({\n")
+  else
+    file:write(varname.." = {\n")
+  end
+
+  for k,v in pairs(lang_tables[lang]) do
+    file:write("\t[\""..k.."\"] = \""..v.."\",\n")
+  end
+
+  if lang ~= "enUS" then
+    file:write("}, {__index="..varname.."})\n")
+  else
+    file:write("}\n")
+  end
+end
