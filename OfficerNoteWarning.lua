c67607518dde152ebf2339f175136e6f0b98d02e
evlogimenos <devnull@localhost>
2008-11-10 19:21:09 +0000

    Fix the officer note warning to not allow the edit of a note unless a user clicks accept.

--- OfficerNoteWarning.lua
+++ OfficerNoteWarning.lua
@@ -8,8 +8,9 @@ function mod:OnInitialize()
     button1 = YES,
     button2 = NO,
     timeout = 0,
-    OnAccept = function()
-      self.hooks[GuildMemberOfficerNoteBackground]["OnMouseUp"]()
+    OnAccept = function(self)
+                 self:Hide()
+                 mod.hooks[GuildMemberOfficerNoteBackground]["OnMouseUp"]()
     end,
     whileDead = 1,
     hideOnEscape = 1,
@@ -22,6 +23,6 @@ end
 
 function mod:OnEnable()
   if GuildMemberOfficerNoteBackground and GuildMemberOfficerNoteBackground:HasScript("OnMouseUp") then
-    self:HookScript(GuildMemberOfficerNoteBackground, "OnMouseUp")
+    self:RawHookScript(GuildMemberOfficerNoteBackground, "OnMouseUp")
   end
 end
