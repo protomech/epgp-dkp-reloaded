80c61fb79670bf26c1443c7afb6ce08090983eb5
d43m0n <devnull@localhost>
2008-11-11 00:36:23 +0000

    Binded the sideframe buttons 'Credit GPs' and 'Award EPs' to the appropriate functions.

--- UI.lua
+++ UI.lua
@@ -560,11 +560,21 @@ local function CreateEPGPSideFrame(self)
   gpFrame:SetPoint("TOPLEFT", f, "TOPLEFT", 15, -30)
   gpFrame:SetPoint("TOPRIGHT", f, "TOPRIGHT", -15, -30)
   AddGPControls(gpFrame)
+  gpFrame.button:SetScript("OnClick", function(self)
+                                        EPGP:IncGPBy(f.row.name, 
+                                                     UIDropDownMenu_GetText(gpFrame.dropDown),
+                                                     gpFrame.editBox:GetNumber())
+                                      end)
 
   local epFrame = CreateFrame("Frame", nil, f)
   epFrame:SetPoint("TOPLEFT", gpFrame, "BOTTOMLEFT", 0, -15)
   epFrame:SetPoint("TOPRIGHT", gpFrame, "BOTTOMRIGHT", 0, -15)
   AddEPControls(epFrame)
+  epFrame.button:SetScript("OnClick", function(self)
+                                        EPGP:IncEPBy(f.row.name, 
+                                                     UIDropDownMenu_GetText(epFrame.dropDown),
+                                                     epFrame.editBox:GetNumber())
+                                      end)
 
   f:SetScript("OnShow",
               function(self) 
