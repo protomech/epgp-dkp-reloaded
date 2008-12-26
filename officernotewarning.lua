4c8012180f9ed0582e55bf3afdfa3aad6cbfcf09
evlogimenos <devnull@localhost>
2008-12-26 05:22:04 +0000

    Add alert icon to officer note warning popup. Also make it accept on enter. This fixes issue 282.

--- officernotewarning.lua
+++ officernotewarning.lua
@@ -2,20 +2,20 @@ local mod = EPGP:NewModule("EPGP_OfficerNoteWarning", "AceHook-3.0")
 
 local L = LibStub("AceLocale-3.0"):GetLocale("EPGP")
 
-function mod:OnInitialize()
-  StaticPopupDialogs["EPGP_OFFICER_NOTE_WARNING"] = {
-    text = L["EPGP is using Officer Notes for data storage. Do you really want to edit the Officer Note by hand?"],
-    button1 = YES,
-    button2 = NO,
-    timeout = 0,
-    OnAccept = function(self)
-                 self:Hide()
-                 mod.hooks[GuildMemberOfficerNoteBackground]["OnMouseUp"]()
-    end,
-    whileDead = 1,
-    hideOnEscape = 1,
-  }
-end
+StaticPopupDialogs["EPGP_OFFICER_NOTE_WARNING"] = {
+  text = L["EPGP is using Officer Notes for data storage. Do you really want to edit the Officer Note by hand?"],
+  button1 = YES,
+  button2 = NO,
+  timeout = 0,
+  OnAccept = function(self)
+               self:Hide()
+               mod.hooks[GuildMemberOfficerNoteBackground]["OnMouseUp"]()
+             end,
+  whileDead = 1,
+  hideOnEscape = 1,
+  showAlert = 1,
+  enterClicksFirstButton = 1,
+}
 
 function mod:OnMouseUp()
   StaticPopup_Show("EPGP_OFFICER_NOTE_WARNING")
