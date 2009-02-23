7494126fda189b16dd747205655b5a7e8471a46a
evlogimenos <devnull@localhost>
2009-02-23 23:04:16 +0000

    Rewrite LibGuildStorage into a state machine that writes back its state lazily. This fixes issue 308.

--- LibGuildStorage-1.0.lua
+++ LibGuildStorage-1.0.lua
@@ -43,6 +43,13 @@ local MINOR_VERSION = tonumber(("$Revision$"):match("%d+")) or 0
 local lib, oldMinor = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
 if not lib then return end
 
+local function Debug(fmt, ...)
+  DEFAULT_CHAT_FRAME:AddMessage(string.format(fmt, ...))
+end
+
+-- Comment out this line to enable debug info
+function Debug(...) end
+
 local CallbackHandler = LibStub("CallbackHandler-1.0")
 if not lib.callbacks then
   lib.callbacks = CallbackHandler:New(lib)
@@ -57,187 +64,54 @@ else
   lib.frame = CreateFrame("Frame", MAJOR_VERSION .. "_Frame")
 end
 local frame = lib.frame
-frame:Hide()
--- Possible states: STALE, LOCAL_PENDING, REMOTE_PENDING,
--- REMOTE_FLUSHED, CURRENT
-local state = "STALE"
-local initialized = false
-
-local timers = LibStub("AceTimer-3.0")
-
--- We want to not call GuildRoster continuously if we are doing a lot
--- of changes so delay it by 10 milliseconds. If another request comes
--- in to update the roster in the meantime cancel the current one and
--- schedule another after 10 milliseconds.
-local guildroster_timer
-local function GuildRosterDelayed()
-  timers:CancelTimer(guildroster_timer, true)
-  guildroster_timer = timers:ScheduleTimer(GuildRoster, 0.01)
-end
-
+frame:Show()
 frame:SetScript("OnEvent",
-                function(this, event, ...)
+                function(self, event, ...)
                   lib[event](lib, ...)
                 end)
 
--- Cache is indexed by name and a table with index, class and note
+local SetState
+
+-- state of the cache: UNINITIALIZED, STALE,
+-- STALE_WAITING_FOR_ROSTER_UPDATE, CURRENT, FLUSHING, REMOTE_FLUSHING
+--
+-- A complete graph of state changes is found in LibGuildStorage-1.0.dot
+local state = "STALE_WAITING_FOR_ROSTER_UPDATE"
+local initialized
+local index
+-- name -> {note=, pending_note=, seen=, class=}
 local cache = {}
 local guild_info = ""
 
-local next_index = 1
-local function UpdateGuildRoster()
-  -- Sometimes GetNumGuildMembers returns 0. In this case return so
-  -- that we call it again and get a proper value.
-  if GetNumGuildMembers(true) == 0 then return end
-
-  if next_index == 1 then
-    local new_guild_info = GetGuildInfoText() or ""
-    if new_guild_info ~= guild_info then
-      guild_info = new_guild_info
-      callbacks:Fire("GuildInfoChanged", guild_info)
-    end
-  end
-
-  -- Read up to 100 members at a time.
-  local e = math.min(next_index + 99, GetNumGuildMembers(true))
-  for i = next_index, e do
-    local name, _, _, _, _, _, _, note, _, _, class = GetGuildRosterInfo(i)
-    if name then
-      local t = cache[name]
-      if not t then
-        t = {}
-        cache[name] = t
-      end
-      -- Mark the entry so that we know we should keep it.
-      t.marked = true
-      t.index = i
-      t.class = class
-      if t.note ~= note then
-        t.note = note
-        -- We want to delay all GuildNoteChanged calls until we have a
-        -- complete view of the guild, otherwise alts might not be
-        -- rejected (we read alts note before we even know about the
-        -- main).
-        if initialized then
-          callbacks:Fire("GuildNoteChanged", name, note)
-        end
-      end
-    end
-  end
-  next_index = e + 1
-  if next_index > GetNumGuildMembers(true) then
-    -- We are done, we need to clear the marks and delete the unmarked
-    -- entries. We also fire events for removed members now.
-    for name, t in pairs(cache) do
-      if t.marked then
-        t.marked = nil
-      else
-        cache[name] = nil
-        callbacks:Fire("GuildNoteDeleted", name)
-      end
-    end
-
-    frame:Hide()
-    if not initialized then
-      initialized = true
-      -- Now make all GuildNoteChanged calls because we have a full
-      -- state.
-      for name, t in pairs(cache) do
-        callbacks:Fire("GuildNoteChanged", name, t.note)
-      end
-    end
-    if state == "STALE" then
-      state = "CURRENT"
-      callbacks:Fire("StateChanged")
-    elseif state == "LOCAL_PENDING" then
-      state = "CURRENT"
-      callbacks:Fire("StateChanged")
-      SendAddonMessage("EPGP", "CHANGES_FLUSHED", "GUILD")
-    elseif state == "REMOTE_FLUSHED" then
-      state = "CURRENT"
-      callbacks:Fire("StateChanged")
-    end      
-  end
-end
-
-frame:SetScript("OnUpdate", UpdateGuildRoster)
-
-frame:RegisterEvent("PLAYER_GUILD_UPDATE")
-frame:RegisterEvent("GUILD_ROSTER_UPDATE")
-frame:RegisterEvent("CHAT_MSG_ADDON")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-
-function lib:CHAT_MSG_ADDON(prefix, msg, type, sender)
-  if prefix == "EPGP" and sender ~= UnitName("player") then
-    if msg == "CHANGES_PENDING" then
-      state = "REMOTE_PENDING"
-      callbacks:Fire("StateChanged")
-    elseif msg == "CHANGES_FLUSHED" then
-      state = "REMOTE_FLUSHED"
-      callbacks:Fire("StateChanged")
-      guildroster_timer = timers:ScheduleTimer(GuildRoster, 10)
-    end
-  end
-end
-
-function lib:PLAYER_GUILD_UPDATE()
-  -- Hide the frame to stop OnUpdate from reading guild information
-  if frame:IsShown() and not IsInGuild() then
-    frame:Hide()
-  end
-  GuildRosterDelayed()
-end
-
-function lib:GUILD_ROSTER_UPDATE(loc)
-  if loc then
-    GuildRosterDelayed()
-    return
-  end
-
-  -- Show the frame to make the OnUpdate handler to be called
-  next_index = 1
-  frame:Show()
-end
-
 function lib:GetNote(name)
-  local entry = cache[name]
-  if not entry then
-    return nil
-  end
-  return entry.note
+  local e = cache[name]
+  if e then return e.note end
 end
 
 function lib:SetNote(name, note)
-  local entry = cache[name]
-  if not entry then
-    return
-  end
-
-  -- Also lock down all other clients as well
-  if state == "CURRENT" then
-    state = "LOCAL_PENDING"
-    callbacks:Fire("StateChanged")
-    SendAddonMessage("EPGP", "CHANGES_PENDING", "GUILD")
-  end
-
-  -- We do not update the note here. We are going to wait until the
-  -- next GUILD_ROSTER_UPDATE and fire a GuildNoteChanged callback.
-
-  if select(1, GetGuildRosterInfo(entry.index)) ~= name then
-    DEFAULT_CHAT_FRAME:AddMessage(
-      string.format("Failed to change %s's note to %s. Please report "..
-                    "this at http://epgp.googlecode.com", name, note))
-  else
-    GuildRosterSetOfficerNote(entry.index, note)
+  local e = cache[name]
+  if e then
+    if e.pending_note then
+      DEFAULT_CHAT_FRAME:AddMessage(
+        string.format("Ignoring attempt to set note before flushing pending "..
+                      "note for %s! "..
+                      "current=[%s] pending=[%s] new[%s]. "..
+                      "Please report this bug along with the actions that "..
+                      "lead to this on http://epgp.googlecode.com",
+                    tostring(name),
+                    tostring(e.note),
+                    tostring(e.pending_note),
+                    tostring(note)))
+    else
+      e.pending_note = note
+      SetState("FLUSHING")
+    end
   end
 end
 
 function lib:GetClass(name)
-  local entry = cache[name]
-  if not entry then
-    return
-  end
-  return entry.class
+  local e = cache[name]
+  if e then return e.class end
 end
 
 function lib:GetGuildInfo()
@@ -264,11 +138,198 @@ function lib:Rollback(t)
   for _,i in pairs(t.roster_info) do
     lib:SetNote(i[1], i[3])
   end
-  GuildRosterDelayed()
+end
+
+--
+-- Event handlers
+--
+frame:RegisterEvent("PLAYER_GUILD_UPDATE")
+frame:RegisterEvent("GUILD_ROSTER_UPDATE")
+frame:RegisterEvent("CHAT_MSG_ADDON")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+function lib:CHAT_MSG_ADDON(prefix, msg, type, sender)
+  if prefix == "EPGP" and sender ~= UnitName("player") then
+    if msg == "CHANGES_PENDING" then
+      SetState("REMOTE_FLUSHING")
+    elseif msg == "CHANGES_FLUSHED" then
+      SetState("STALE_WAITING_FOR_ROSTER_UPDATE")
+    end
+  end
+end
+
+function lib:PLAYER_GUILD_UPDATE()
+  if IsInGuild() then
+    frame:Show()
+  else
+    frame:Hide()
+  end
+  SetState("STALE_WAITING_FOR_ROSTER_UPDATE")
 end
 
 function lib:PLAYER_ENTERING_WORLD()
-  frame:Hide()
-  GuildRosterDelayed()
-  last_guildroster_time = GetTime()
+  lib:PLAYER_GUILD_UPDATE()
 end
+
+function lib:GUILD_ROSTER_UPDATE(loc)
+  Debug("GUILD_ROSTER_UPDATE(%s)", tostring(loc))
+  if loc then
+    SetState("FLUSHING") -- SetState("STALE_WAITING_FOR_ROSTER_UPDATE")
+  else
+    if state ~= "UNINITIALIZED" then
+      SetState("STALE")
+      index = nil
+    end
+  end
+end
+
+--
+-- Locally defined functions
+--
+
+local valid_transitions = {
+  UNINITIALIZED = {
+    CURRENT = true,
+  },
+  STALE = {
+    CURRENT = true,
+    REMOTE_FLUSHING = true,
+    STALE_WAITING_FOR_ROSTER_UPDATE = true,
+  },
+  STALE_WAITING_FOR_ROSTER_UPDATE = {
+    STALE = true,
+    FLUSHING = true,
+  },
+  CURRENT = {
+    FLUSHING = true,
+    REMOTE_FLUSHING = true,
+    STALE = true,
+  },
+  FLUSHING = {
+    STALE_WAITING_FOR_ROSTER_UPDATE = true,
+  },
+  REMOTE_FLUSHING = {
+    STALE_WAITING_FOR_ROSTER_UPDATE = true,
+  },
+}
+
+function SetState(new_state)
+  if state == new_state then return end
+
+  if not valid_transitions[state][new_state] then
+    Debug("Ignoring state change %s -> %s", state, new_state)
+    return
+  else
+    Debug("StateChanged: %s -> %s", state, new_state)
+    state = new_state
+    if new_state == FLUSHING then
+      SendAddonMessage("EPGP", "CHANGES_PENDING", "GUILD")
+    end
+    callbacks:Fire("StateChanged")
+  end
+end
+
+local function Frame_OnUpdate(self, elapsed)
+  if state == "CURRENT" then
+    return
+  end
+
+  if state == "STALE_WAITING_FOR_ROSTER_UPDATE" then
+    GuildRoster()
+    return
+  end
+
+  -- Sometimes GetNumGuildMembers returns 0. In this case return now,
+  -- so that we call it again and get a proper value.
+  if GetNumGuildMembers(true) == 0 then return end
+
+  if not index or index >= GetNumGuildMembers(true) then
+    index = 1
+  end
+
+  -- Check guild info for changes.
+  if index == 1 then
+    local new_guild_info = GetGuildInfoText() or ""
+    if new_guild_info ~= guild_info then
+      guild_info = new_guild_info
+      callbacks:Fire("GuildInfoChanged", guild_info)
+    end
+  end
+
+  -- Read up to 50 members at a time.
+  local last_index = math.min(index + 50, GetNumGuildMembers(true))
+  Debug("Processing from %d to %d members", index, last_index)
+
+  for i = index, last_index do
+    local name, _, _, _, _, _, _, note, _, _, class = GetGuildRosterInfo(i)
+    if name then
+      local entry = cache[name]
+      if not entry then
+        entry = {}
+        cache[name] = entry
+      end
+
+      -- Mark this note as seen
+      entry.seen = true
+      if entry.note ~= note then
+        entry.note = note
+        entry.class = class
+        -- We want to delay all GuildNoteChanged calls until we have a
+        -- complete view of the guild, otherwise alts might not be
+        -- rejected (we read alts note before we even know about the
+        -- main).
+        if initialized then
+          callbacks:Fire("GuildNoteChanged", name, note)
+        end
+        if entry.pending_note then
+          callbacks:Fire("InconsistentNote",
+                         name, note, entry.note, entry.pending_note)
+        end
+      end
+
+      if entry.pending_note then
+        GuildRosterSetOfficerNote(i, entry.pending_note)
+        entry.pending_note = nil
+      end
+    end
+  end
+  index = last_index + 1
+  if index > GetNumGuildMembers(true) then
+    -- We are done, we need to clear the seen marks and delete the
+    -- unmarked entries. We also fire events for removed members now.
+    for name, t in pairs(cache) do
+      if t.seen then
+        t.seen = nil
+      else
+        cache[name] = nil
+        callbacks:Fire("GuildNoteDeleted", name)
+      end
+    end
+
+    if not initialized then
+      -- Now make all GuildNoteChanged calls because we have a full
+      -- state.
+      for name, t in pairs(cache) do
+        callbacks:Fire("GuildNoteChanged", name, t.note)
+      end
+    end
+    if state == "STALE" then
+      SetState("CURRENT")
+    elseif state == "FLUSHING" then
+      local flushed = true
+      for name, t in pairs(cache) do
+        if t.pending_note then
+          flushed = false
+          break
+        end
+      end
+      if flushed then
+        SetState("STALE_WAITING_FOR_ROSTER_UPDATE")
+        SendAddonMessage("EPGP", "CHANGES_FLUSHED", "GUILD")
+      end
+    end      
+  end
+end
+
+frame:SetScript("OnUpdate", Frame_OnUpdate)
+GuildRoster()
