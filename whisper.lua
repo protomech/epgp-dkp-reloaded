8858c4796a5862107e60aec95a936dbbdd12b195
evlogimenos <devnull@localhost>
2009-03-11 23:35:15 +0000

    Fix standby awards. This fixes issue 330.

--- whisper.lua
+++ whisper.lua
@@ -6,7 +6,6 @@ local senderMap = {}
 
 function mod:CHAT_MSG_WHISPER(event_name, msg, sender)
   if not UnitInRaid("player") then return end
-  if not EPGP.db.profile.auto_standby_whispers then return end
   if not msg:match("epgp standby") then return end
 
   local member = msg:match("epgp standby ([^ ]+)")
@@ -36,14 +35,10 @@ local function SendNotifiesAndClearExtras(event_name, names, reason, amount)
     if EPGP:IsMemberInExtrasList(member) then
       SendChatMessage(L["%+d EP (%s) to %s"]:format(amount, reason, member),
                       "WHISPER", nil, sender)
-      -- If whispers are enabled clear the standby list so that people
-      -- can re-add themselves.
-      if EPGP.db.profile.auto_standby_whispers then
-        SendChatMessage(
-          L["%s is now removed from the award list"]:format(member),
-          "WHISPER", nil, sender)
-        EPGP:DeSelectMember(member)
-      end
+      SendChatMessage(
+        L["%s is now removed from the award list"]:format(member),
+        "WHISPER", nil, sender)
+      EPGP:DeSelectMember(member)
     end
     senderMap[member] = nil
   end
