9d484d8c4a4e061b4b7115b01449237be5c045bd
evlogimenos <devnull@localhost>
2009-03-29 04:52:02 +0000

    Make announces more granular.
    This fixes issue 296.

--- announce.lua
+++ announce.lua
@@ -1,5 +1,6 @@
 local mod = EPGP:NewModule("announce")
 
+local Debug = LibStub("LibDebug-1.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("EPGP")
 
 function mod:Announce(fmt, ...)
@@ -31,17 +32,17 @@ function mod:Announce(fmt, ...)
   SendChatMessage(str, medium, nil, GetChannelName(channel))
 end
 
-local function AnnounceEPAward(event_name, name, reason, amount, mass)
+function mod:EPAward(event_name, name, reason, amount, mass)
   if mass then return end
   mod:Announce(L["%+d EP (%s) to %s"], amount, reason, name)
 end
 
-local function AnnounceGPAward(event_name, name, reason, amount, mass)
+function mod:GPAward(event_name, name, reason, amount, mass)
   if mass then return end
   mod:Announce(L["%+d GP (%s) to %s"], amount, reason, name)
 end
 
-local function AnnounceMassEPAward(event_name, names, reason, amount)
+function mod:MassEPAward(event_name, names, reason, amount)
   local first = true
   local awarded
 
@@ -57,25 +58,25 @@ local function AnnounceMassEPAward(event_name, names, reason, amount)
   mod:Announce(L["%+d EP (%s) to %s"], amount, reason, awarded)
 end
 
-local function AnnounceDecay(event_name, decay_p)
+function mod:Decay(event_name, decay_p)
   mod:Announce(L["Decay of EP/GP by %d%%"], decay_p)
 end
 
-local function AnnounceStartRecurringAward(event_name, reason, amount, mins)
+function mod:StartRecurringAward(event_name, reason, amount, mins)
   local fmt, val = SecondsToTimeAbbrev(mins * 60)
   mod:Announce(L["Start recurring award (%s) %d EP/%s"], reason, amount, fmt:format(val))
 end
 
-local function AnnounceResumeRecurringAward(event_name, reason, amount, mins)
+function mod:ResumeRecurringAward(event_name, reason, amount, mins)
   local fmt, val = SecondsToTimeAbbrev(mins * 60)
   mod:Announce(L["Resume recurring award (%s) %d EP/%s"], reason, amount, fmt:format(val))
 end
 
-local function AnnounceStopRecurringAward(event_name)
+function mod:StopRecurringAward(event_name)
   mod:Announce(L["Stop recurring award"])
 end
 
-local function AnnounceEPGPReset(event_name)
+function mod:EPGPReset(event_name)
   mod:Announce(L["EP/GP are reset"])
 end
 
@@ -83,6 +84,9 @@ mod.dbDefaults = {
   profile = {
     enabled = true,
     medium = "GUILD",
+    events = {
+      ['*'] = true,
+    },
   }
 }
 
@@ -92,7 +96,7 @@ mod.optionsArgs = {
   help = {
     order = 1,
     type = "description",
-    name = L["Announces all EPGP actions to the specified medium."],
+    name = L["Announces EPGP actions to the specified medium."],
   },
   medium = {
     order = 10,
@@ -114,19 +118,48 @@ mod.optionsArgs = {
     desc = L["Sets the custom announce channel name used to announce EPGP actions."],
     disabled = function(i) return mod.db.profile.medium ~= "CHANNEL" end,
   },
+  events = {
+    order = 12,
+    type = "multiselect",
+    name = L["Announce when:"],
+    values = {
+      EPAward = L["A member is awarded EP"],
+      MassEPAward = L["Guild or Raid are awarded EP"],
+      GPAward = L["A member is credited GP"],
+      Decay = L["EPGP decay"],
+      StartRecurringAward = L["Recurring awards start"],
+      StopRecurringAward = L["Recurring awards stop"],
+      ResumeRecurringAward = L["Recurring awards resume"],
+      EPGPReset = L["EPGP reset"],
+    },
+    width = "full",
+    get = "GetEvent",
+    set = "SetEvent",
+  },
 }
 
+function mod:GetEvent(i, e)
+  return self.db.profile.events[e]
+end
+
+function mod:SetEvent(i, e, v)
+  if v then
+    Debug("Enabling announce of: %s", e)
+    EPGP.RegisterCallback(self, e)
+  else
+    Debug("Disabling announce of: %s", e)
+    EPGP.UnregisterCallback(self, e)
+  end
+  self.db.profile.events[e] = v
+end
+
 function mod:OnEnable()
-  EPGP.RegisterCallback(self, "EPAward", AnnounceEPAward)
-  EPGP.RegisterCallback(self, "MassEPAward", AnnounceMassEPAward)
-  EPGP.RegisterCallback(self, "GPAward", AnnounceGPAward)
-  EPGP.RegisterCallback(self, "Decay", AnnounceDecay)
-  EPGP.RegisterCallback(self, "StartRecurringAward",
-                        AnnounceStartRecurringAward)
-  EPGP.RegisterCallback(self, "ResumeRecurringAward",
-                        AnnounceResumeRecurringAward)
-  EPGP.RegisterCallback(self, "StopRecurringAward", AnnounceStopRecurringAward)
-  EPGP.RegisterCallback(self, "EPGPReset", AnnounceEPGPReset)
+  for e, _ in pairs(mod.optionsArgs.events.values) do
+    if self.db.profile.events[e] then
+      Debug("Enabling announce of: %s (startup)", e)
+      EPGP.RegisterCallback(self, e)
+    end
+  end
 end
 
 function mod:OnDisable()
