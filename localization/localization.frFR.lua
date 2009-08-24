acd0b9fd711147e2039426eb545014aa5032bf44
evlogimenos <devnull@localhost>
2009-08-24 22:22:46 +0000

    Update localizations.

--- localization/localization.frFR.lua
+++ localization/localization.frFR.lua
@@ -40,9 +40,9 @@ L["EPGP reset"] = "R\195\169initialiser EPGP"
 L["EP Reason"] = "Raison de l'EP"
 L["Export"] = "Exporter"
 L["Extras Percent should be a number between 0 and 100"] = "Pourcentage suppl\195\169mentaire devrait \195\170tre un nombre compris entre 0 et 100"
-L["GP: %d"] = "GP: %cl" -- Needs review
+L["GP: %d"] = "GP: %d"
 L["GP: %d [ItemLevel=%d]"] = "GP : %d [NiveauObjet=%d]"
-L["GP: %d or %d"] = "GP: %d or %d" -- Needs review
+L["GP: %d or %d"] = "GP: %d ou %d"
 L["GP: %d or %d [ItemLevel=%d]"] = "GP : %d ou %d [NiveauObjet=%d]"
 L["GP on tooltips"] = "GP sur les Tooltips"
 L["GP Reason"] = "Raison du GP"
@@ -88,13 +88,13 @@ L["%s is dead. Award EP?"] = "%s est mort. Gain d'EP ?"
 L["%s is not eligible for EP awards"] = "%s n'est pas \195\169ligible pour les gains d'EP"
 L["%s is now removed from the award list"] = "%s est \195\160 pr\195\169sent supprim\195\169 de la liste des gains"
 L["%s: %s to %s"] = "a\
-depends of the sentence" -- Needs review
+depends of the sentence"
 L["Standby"] = "En attente"
 L["Standby whispers in raid"] = "Svp patcienter message priv\195\169 dans le raid"
 L["Start recurring award (%s) %d EP/%s"] = "Commencer la collecte des gains (%s) %d EP/%s"
 L["Stop recurring award"] = "Arr\195\170ter la collecte des gains"
 L["%s to %s"] = "a\
-depends of the sentence" -- Needs review
+depends of the sentence"
 L["The imported data is invalid"] = "Les donn\195\169es import\195\169es ne sont pas valides"
 L["To export the current standings, copy the text below and post it to: %s"] = "Pour exporter le classement actuel, copier le texte suivant et copier le sur: %s"
 L["Tooltip"] = "Tooltip"
@@ -105,8 +105,8 @@ L["Undo"] = "Annuler"
 L["Using DBM for boss kill tracking"] = "Utiliser DBM pour suivre la mort des boss"
 L["Value"] = "Valeur"
 L["Whisper"] = "Chuchoter"
-L["Wipe awards"] = "R\195\169compenses de wipe" -- Needs review
-L["Wiped on %s. Award EP?"] = "Wipe sur %8. Attribuer EP?" -- Needs review
+L["Wipe awards"] = "R\195\169compenses de wipe"
+L["Wiped on %s. Award EP?"] = "Wipe sur %s. Attribuer EP?"
 L["You cannot undo this action!"] = "Vous ne pouvez pas annuler cette action!"
-L["You can now check your epgp standings and loot on the web: http://www.epgpweb.com"] = "Vous pouvez maintenant v\195\169rifier votre rang EPGP et loot sur le web: http://epgpweb.com" -- Needs review
+L["You can now check your epgp standings and loot on the web: http://www.epgpweb.com"] = "Vous pouvez maintenant v\195\169rifier votre rang EPGP et loot sur le web: http://epgpweb.com"
 L["You can now check your epgp standings on the web: http://www.epgpweb.com"] = "Vous pouivez maintenant verifer les priorit\195\169es de loot a l'adresse http://www.epgpweb.com/"
