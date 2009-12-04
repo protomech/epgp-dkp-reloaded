b74ded58c48e1d567234e76d29420795ab2a5f80
evlogimenos <devnull@localhost>
2009-12-04 01:15:34 +0000

    Add command line interface to EPGP to accomodate UI haters!
    Fixes issue 329.

--- epgp_options.lua
+++ epgp_options.lua
@@ -1,4 +1,5 @@
 local L = LibStub("AceLocale-3.0"):GetLocale("EPGP")
+local GP = LibStub("LibGearPoints-1.0")
 local Debug = LibStub("LibDebug-1.0")
 
 function EPGP:SetupOptions()
@@ -38,6 +39,12 @@ function EPGP:SetupOptions()
     },
   }
 
+  local registry = LibStub("AceConfigRegistry-3.0")
+  registry:RegisterOptionsTable("EPGP Options", options)
+
+  local dialog = LibStub("AceConfigDialog-3.0")
+  dialog:AddToBlizOptions("EPGP Options", "EPGP")
+
   -- Setup options for each module that defines them.
   for name, m in self:IterateModules() do
     if m.optionsArgs then
@@ -64,40 +71,80 @@ function EPGP:SetupOptions()
       }
     end
     if m.optionsName then
-      -- Add this module's options.
-      options.args[name] = {
-        handler = m,
-        order = 100,
-        type = "group",
-        name = m.optionsName,
-        desc = m.optionsDesc,
-        args = m.optionsArgs,
-        get = "GetDBVar",
-        set = "SetDBVar",
-      }
+      registry:RegisterOptionsTable("EPGP " .. name, {
+                                      handler = m,
+                                      order = 100,
+                                      type = "group",
+                                      name = m.optionsName,
+                                      desc = m.optionsDesc,
+                                      args = m.optionsArgs,
+                                      get = "GetDBVar",
+                                      set = "SetDBVar",
+                                    })
+      dialog:AddToBlizOptions("EPGP " .. name, m.optionsName, "EPGP")
     end
   end
 
-  local config = LibStub("AceConfig-3.0")
-  local dialog = LibStub("AceConfigDialog-3.0")
+  EPGP:RegisterChatCommand("epgp", "ProcessCommand")
+end
 
-  config:RegisterOptionsTable("EPGP-Bliz", options)
-  dialog:AddToBlizOptions("EPGP-Bliz", "EPGP")
+function EPGP:ProcessCommand(str)
+  local command, nextpos = self:GetArgs(str, 1)
+  if command == "config" then
+    InterfaceOptionsFrame_OpenToCategory("EPGP")
+  elseif command == "debug" then
+    Debug:Toggle()
+  elseif command == "massep" then
+    local reason, amount = self:GetArgs(str, 2, nextpos)
+    amount = tonumber(amount)
+    if self:CanIncEPBy(reason, amount) then
+      self:IncMassEPBy(reason, amount)
+    end
+  elseif command == "ep" then
+    local member, reason, amount = self:GetArgs(str, 3, nextpos)
+    amount = tonumber(amount)
+    if self:CanIncEPBy(reason, amount) then
+      self:IncEPBy(member, reason, amount)
+    end
+  elseif command == "gp" then
+    local member, itemlink, amount = self:GetArgs(str, 3, nextpos)
+    self:Print(member, itemlink, amount)
+    if amount then
+      amount = tonumber(amount)
+    else
+      local gp1, gp2 = GP:GetValue(itemlink)
+      self:Print(gp1, gp2)
+      -- Only automatically fill amount if we have a single GP value.
+      if gp1 and not gp2 then
+        amount = gp1
+      end
+    end
 
-  SLASH_EPGP1 = "/epgp"
-  SlashCmdList["EPGP"] = function(msg)
-                           if msg == "config" then
-                             InterfaceOptionsFrame_OpenToCategory("EPGP")
-                           elseif msg == "debug" then
-                             Debug:Toggle()
-                           else
-                             if EPGPFrame and IsInGuild() then
-                               if EPGPFrame:IsShown() then
-                                 HideUIPanel(EPGPFrame)
-                               else
-                                 ShowUIPanel(EPGPFrame)
-                               end
-                             end
-                           end
-                         end
+    if self:CanIncGPBy(itemlink, amount) then
+      self:IncGPBy(member, itemlink, amount)
+    end
+  elseif command == "decay" then
+    if EPGP:CanDecayEPGP() then
+      StaticPopup_Show("EPGP_DECAY_EPGP", EPGP:GetDecayPercent())
+    end
+  elseif command == "help" then
+    local help = {
+      self.version,
+      "   config - "..L["Open the configuration options"],
+      "   debug - "..L["Open the debug window"],
+      "   massep <reason> <amount> - "..L["Mass EP Award"],
+      "   ep <name> <reason> <amount> - "..L["Award EP"],
+      "   gp <name> <itemlink> [<amount>] - "..L["Credit GP"],
+      "   decay - "..L["Decay of EP/GP by %d%%"]:format(EPGP:GetDecayPercent()),
+    }
+    EPGP:Print(table.concat(help, "\n"))
+  else
+    if EPGPFrame and IsInGuild() then
+      if EPGPFrame:IsShown() then
+        HideUIPanel(EPGPFrame)
+      else
+        ShowUIPanel(EPGPFrame)
+      end
+    end
+  end
 end
